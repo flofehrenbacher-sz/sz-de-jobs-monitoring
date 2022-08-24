3a54d1d62449551bdec5e96cc5e4fbceeacb261f
Christoph Herrmann <github@sharaal.de>
2022-08-24 17:25:08 +0200

    Get overview page and checks all job descriptions

---
+++ test/overview.test.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { load } from 'cheerio'
+import * as https from 'https'
+
+export const client = axios.create({
+  httpsAgent: new https.Agent({ keepAlive: true }),
+  validateStatus: () => true,
+})
+
+const overviewPath = 'https://www.sueddeutsche.de/projekte/artikel/verlag/die-zukunft-der-zeitung-e100731/'
+
+describe('Job description', () => {
+  it('get overview page and check all job description links', async () => {
+    const response = await client.get(overviewPath)
+    const $ = load(response.data)
+    await Promise.all($('a').map(async (_, a) => {
+      const href = a.attribs.href
+      if (href?.match(/https:\/\/jobs.swmh.de\/Vacancies\/\d+\/Description\/1/)) {
+        const response = await client.get(href)
+        expect({ href, status: response.status }).toEqual({ href, status: 200 })
+      }
+    }))
+  })
+})
